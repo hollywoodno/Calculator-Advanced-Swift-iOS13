324573f2d76ccc77b11670082321b49f668424be
hollywoodno <hollywoodno@gmail.com>
2020-01-31 20:08:54 -0800

    display numbers type

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,23 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var displayLabel: UILabel!
-    
-    
-    
-    @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
+  
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  private var isFinishedTypingNumbers = true
+  
+  @IBAction func calcButtonPressed(_ sender: UIButton) {
+    isFinishedTypingNumbers = true
+  }
+  
+  @IBAction func numButtonPressed(_ sender: UIButton) {
+    if let numString = sender.currentTitle {
+      
+      if isFinishedTypingNumbers {
+        // starting a new number
+        isFinishedTypingNumbers = false
+        displayLabel.text = numString
+      } else {
+        displayLabel.text = displayLabel.text! + numString
+      }
+      
     }
-
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
-    }
-
+  }
+  
 }
 
